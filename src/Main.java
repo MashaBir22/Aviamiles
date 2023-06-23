d2086b823ff0296778b7edff62baa8d9df6d7294
MashaBir22 <134604130+MashaBir22@users.noreply.github.com>
2023-06-23 17:36:39 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
-  public static void main(String[] args) {
-    int ticketCost = 13676; // Стоимость билета
-    int milesPerRuble = 20; // Количество рублей для одной бонусной мили
-    
-    int bonusMiles = ticketCost / milesPerRuble; // Расчет количества бонусных миль
-    
-    System.out.println("Количество начисленных миль: " + bonusMiles);
-  }
+    public static void main(String[] args) {
+        int price = 15555;
+        int bonus = price / 20;
+
+        System.out.println(bonus);
+    }
 }
